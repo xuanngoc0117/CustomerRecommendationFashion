905ac56a56959d2a31332185307f7100e03daf02
xuanngoc0117 <135565936+xuanngoc0117@users.noreply.github.com>
2024-04-03 12:46:45 +0700

    Update fashion.py

--- fashion.py
+++ fashion.py
@@ -2,14 +2,16 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from underthesea import word_tokenize, pos_tag, sent_tokenize
 import matplotlib.pyplot as plt
 import seaborn as sns
-#from surprise import Reader, Dataset, SVD, SVDpp, NMF, SlopeOne, KNNBasic, KNNBaseline, KNNWithMeans, KNNWithZScore, CoClustering, BaselineOnly
-#from surprise import *
-#from surprise.model_selection.validation import cross_validate
+from surprise import Reader, Dataset, SVD, SVDpp, NMF, SlopeOne, KNNBasic, KNNBaseline, KNNWithMeans, KNNWithZScore, CoClustering, BaselineOnly
+from surprise import *
+from surprise.model_selection.validation import cross_validate
 from gensim import corpora, models, similarities
-#import jieba
+import jieba
 import re
 
 # 1. Read data
