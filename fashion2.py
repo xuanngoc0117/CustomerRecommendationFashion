dda8518146aa87343a720f4e5f66247105d9cb47
xuanngoc0117 <135565936+xuanngoc0117@users.noreply.github.com>
2024-04-05 10:19:28 +0700

    Update fashion2.py

--- fashion2.py
+++ fashion2.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from underthesea import word_tokenize, pos_tag, sent_tokenize
 #import matplotlib.pyplot as plt
 #import seaborn as sns
